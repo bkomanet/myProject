132bac255bf4782f70920e7e513ece634e7b872e
my Name <myEmail@gmail.com>
2018-07-10 10:04:49 -0700

    Added the main.cpp file for our next version

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//Main Program
+//Author: Idunno
+//Date: 2017/06/03
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+	cout << "Welcome to my stupid little program" << endl;	
+	return 0;
+}//main
