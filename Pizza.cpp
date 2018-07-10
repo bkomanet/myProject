18ee8e5f98acb800476bdb5744bfdd32e4a38b06
my Name <myEmail@gmail.com>
2018-07-10 10:50:46 -0700

    Added a Pizza class

---
+++ Pizza.cpp
@@ -0,0 +1,18 @@
+/*
+ * Pizza.cpp
+ *
+ *  Created on: Jul 10, 2018
+ *      Author: bkomanet
+ */
+
+#include "Pizza.hpp"
+
+Pizza::Pizza() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Pizza::~Pizza() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Pizza.hpp
@@ -0,0 +1,17 @@
+/*
+ * Pizza.h
+ *
+ *  Created on: Jul 10, 2018
+ *      Author: bkomanet
+ */
+
+#ifndef PIZZA_HPP_
+#define PIZZA_HPP_
+
+class Pizza {
+public:
+	Pizza();
+	virtual ~Pizza();
+};
+
+#endif /* PIZZA_HPP_ */
